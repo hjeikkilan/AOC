c0e5e0190af53d4069d354ba77b07d5fd776af2c
hjeikkilan <janh709@gmail.com>
2022-12-04 07:26:51 +0200

    Day 4: Python implementation added for both parts

--- day3/day3_2.py
+++ day3/day3_2.py
@@ -6,12 +6,7 @@ file1 = open(r'C:\git\AdventOfCode2022\AOC\day3\input3.txt', 'r')
 lines = file1.readlines()
 
 points = 0
-
-def split(input):
-    length = len(input)
-    return input[0:int(length/2)],input[int(length/2):length]
      
-
 def charToPoints(char):
     if char.isupper():
         return ord(char)-64+26
@@ -19,8 +14,8 @@ def charToPoints(char):
         return ord(char)-96
 
 for i in range(0,len(lines),3):
-    common = list(set(lines[i]).intersection(lines[i+1]).intersection(lines[i+2]))
-    print(common)
+    # common = list(set(lines[i]).intersection(lines[i+1]).intersection(lines[i+2]))
+    common = list(set(lines[i]).intersection(lines[i+1],lines[i+2]))
     common.remove("\n")
     points += charToPoints(common[0])
 
---
+++ day4/day4_1.py
@@ -0,0 +1,33 @@
+import time
+
+start = time.perf_counter()
+# Using readlines()
+file1 = open(r'C:\git\AdventOfCode2022\AOC\day4\input4.txt', 'r')
+Lines = file1.readlines()
+
+points = 0
+
+def split(input):
+    input = input.replace('\n', '')
+    input = input.split(',')
+    return input[0],input[1]
+
+def fillCaps(input):
+    input = input.split('-')
+    newList = []
+    for i in range(int(input[0]),int(input[1])+1,1):
+        newList.append(i)
+    return newList
+
+for line in Lines:
+    a,b = split(line)
+    a,b = fillCaps(a),fillCaps(b)
+    common = list(set(a).intersection(b))
+    if len(common) == len(a) or len(common) == len(b):
+        points += 1
+
+print(points)
+
+end = time.perf_counter()
+
+print(f"Completed in {end - start:0.4f} seconds")
